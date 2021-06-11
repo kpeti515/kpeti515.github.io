e264eb3c7e718a5f00fdb53416fae4d0d70f72ae
Lorand Kallai <lorandkallai@gmail.com>
2021-06-11 21:45:00 +0200

    favico added

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Projects</li>
+        <li>Skills</li>
+        <li>Awards</li>
+        <li>About me</li>
+        <li>Contact</li>
+        <li>Resume</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Header
