8015fa8b48ba15f76ec2d6e28d18cafb70389714
Lorand Kallai <lorandkallai@gmail.com>
2021-06-12 10:49:50 +0200

    react router added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Header from './components/Header'
+import Footer from './components/Footer'
+import { HashRouter as Router } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-        </p>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
