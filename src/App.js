d1bd9d19315d8596bdc4dd863bf101e6d68b421f
Lorand Kallai <lorandkallai@gmail.com>
2021-06-13 10:06:42 +0200

    status 404 changed to path=*

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
           <Route exact path='/about' component={About} />
           <Route exact path='/contact' component={Contact} />
           <Route exact path='/resume' component={Resume} />
-          <Route component={NoMatch} status={404} />
+          <Route path='*' component={NoMatch} />
         </Switch>
         <Footer />
       </div>
